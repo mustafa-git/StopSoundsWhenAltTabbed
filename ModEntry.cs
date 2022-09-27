bcf47cecf484e3b317d931ce15d95d370cca0225
mustafa-git <89876073+mustafa-git@users.noreply.github.com>
2022-09-27 09:47:53 +0200

    Update ModEntry.cs
    Add config option to only stop sounds when the game is set to pause.

--- ModEntry.cs
+++ ModEntry.cs
@@ -5,16 +5,28 @@ using StardewValley;
 
 namespace StopSoundsWhenAltTabbed
 {
+    public class ModConfig
+    {
+        // Config option.
+        public bool OnlyWhenPaused { get; set; } = true;
+    }
     public class ModEntry : Mod
     {
         // Store current settings to restore later.
-        float soundVol;
-        float ambientVol;
-        float footstepVol;
-        bool volumeSaved = false;
+        private float soundVol;
+        private float ambientVol;
+        private float footstepVol;
+        private bool volumeSaved = false;
+        private ModConfig Config = null!;
+        private ModConfig LoadConfig()
+        {
+            var Config = this.Helper.ReadConfig<ModConfig>();
+            return Config;
+        }
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.UpdateTicking += this.OnUpdateTicking;
+            this.Config = this.LoadConfig();
         }
         void OnUpdateTicking(object? sender, UpdateTickingEventArgs e)
         {
@@ -22,23 +34,40 @@ namespace StopSoundsWhenAltTabbed
             {
                 if (!Game1.game1.IsActive)
                 {
-                    Game1.currentSong.Pause();
-                    if (!volumeSaved)
+                    if (Config.OnlyWhenPaused)
                     {
-                        soundVol = Game1.options.soundVolumeLevel;
-                        ambientVol = Game1.options.ambientVolumeLevel;
-                        footstepVol = Game1.options.footstepVolumeLevel;
-                        volumeSaved = true;
+                        if (!volumeSaved && Game1.options.pauseWhenOutOfFocus == true)
+                        {
+                            soundVol = Game1.options.soundVolumeLevel;
+                            ambientVol = Game1.options.ambientVolumeLevel;
+                            footstepVol = Game1.options.footstepVolumeLevel;
+                            Game1.currentSong.Pause();
+                            Game1.soundCategory.SetVolume(0.0f);
+                            Game1.ambientCategory.SetVolume(0.0f);
+                            Game1.footstepCategory.SetVolume(0.0f);
+                            volumeSaved = true;
+                        }
                     }
-                    Game1.soundCategory.SetVolume(0.0f);
-                    Game1.ambientCategory.SetVolume(0.0f);
-                    Game1.footstepCategory.SetVolume(0.0f);
+                    else
+                    {
+                        if (!volumeSaved)
+                        {
+                            soundVol = Game1.options.soundVolumeLevel;
+                            ambientVol = Game1.options.ambientVolumeLevel;
+                            footstepVol = Game1.options.footstepVolumeLevel;
+                            Game1.currentSong.Pause();
+                            Game1.soundCategory.SetVolume(0.0f);
+                            Game1.ambientCategory.SetVolume(0.0f);
+                            Game1.footstepCategory.SetVolume(0.0f);
+                            volumeSaved = true;
+                        }
+                    }
                 }
                 else
                 {
-                    Game1.currentSong.Resume();
                     if (volumeSaved)
                     {
+                        Game1.currentSong.Resume();
                         Game1.soundCategory.SetVolume(soundVol);
                         Game1.ambientCategory.SetVolume(ambientVol);
                         Game1.footstepCategory.SetVolume(footstepVol);
